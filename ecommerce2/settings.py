189046e76cb2f7f9dda42bd262e6f1977173cf50
babor99 <islambabor947@gmail.com>
2021-03-05 20:29:07 +0600

    second commit

--- ecommerce2/settings.py
+++ ecommerce2/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
